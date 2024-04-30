84b9646b79f718e755fbfd2a7db1acbb0a195792
wnourse05 <wrn13@case.edu>
2024-04-30 11:06:51 -0400

    Ran with 1 filter

--- train_net_backprop.py
+++ train_net_backprop.py
@@ -16,8 +16,8 @@ from tqdm import tqdm
 
 start = time.time()
 N_STEPS = 90
-BATCH_SIZE = 24
-N_EPOCHS = 50
+BATCH_SIZE = 32
+N_EPOCHS = 100
 DT = ((1/30)/13)*1000
 IMG_SIZE = [24,64]
 FIELD_SIZE = 5
@@ -34,6 +34,19 @@ def vel_to_state(vel, device):
     state[:,0] = -2*vel
     state[:,1] = 2*vel
     return torch.clamp(state, min=0)
+
+def accuracy(states, labels, device):
+    states = torch.diff(states, dim=1).flatten()
+    labels = torch.diff(labels, dim=1).flatten()
+    targets = torch.tensor(2*[-0.5, -0.45, -0.4, -0.35, -0.3, -0.25, -0.2, -0.15, -0.1, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5], device=device)
+    diff = targets.unsqueeze(0) - states.unsqueeze(1)
+    abs_diff = torch.abs(diff)
+    min_idx = torch.argmin(abs_diff, dim=1)
+    rounded = targets[min_idx]
+    num_correct = torch.sum(rounded == labels)
+    total = len(labels)
+    return num_correct, total
+
 
 # vel = torch.tensor([-0.5, -0.45, -0.4, -0.35, -0.3, -0.25, -0.2, -0.15, -0.1, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5])
 # out = vel_to_state(vel)
@@ -53,6 +66,7 @@ for r in range(N_SEEDS):
     test_loss_last = 0.0
     loss_history = []
     loss_test_history = []
+    accuracy_history = []
     # for epoch in range(N_EPOCHS):  # loop over the dataset multiple times
     epoch = 0
     while test_loss < test_loss_last or epoch <= N_EPOCHS:
@@ -95,6 +109,8 @@ for r in range(N_SEEDS):
         loss_history.extend([train_running_loss/(i+1)])
         test_loss_last = test_loss
         test_loss = 0.0
+        num_correct = 0
+        total = 0
         with torch.no_grad():
             model.setup()
             # optimizer.zero_grad()
@@ -109,13 +125,17 @@ for r in range(N_SEEDS):
                 outputs = model(inputs, states)
                 placeholder = criterion(outputs, targets)
                 test_loss += placeholder.detach().item()
+                a, b = accuracy(outputs, targets, device)
+                num_correct += a
+                total += b
             loss_test_history.extend([test_loss/(i+1)])
+            accuracy_history.extend([100*num_correct/total])
         epoch += 1
 
-        print('Test Loss: %.4f | Time: %i secs' % (test_loss / (i+1), time.time()-start))
+        print('Test Loss: %.4f | Test Accuracy: %.2f%% | Time: %i secs' % (test_loss / (i+1), 100*num_correct/total, time.time()-start))
         torch.save(model.state_dict(),'1-CHECKPT-'+datetime.now().strftime('%Y-%m-%d-%H-%M-%S')+'.pt')
 
-        save_data = {'loss': loss_history, 'lossTest': loss_test_history}
+        save_data = {'loss': loss_history, 'lossTest': loss_test_history, 'accuracy': accuracy_history}
         pickle.dump(save_data, open('1-LOSS-'+datetime.now().strftime('%Y-%m-%d-%H-%M-%S')+'.p', 'wb'))
 
     plt.figure()
